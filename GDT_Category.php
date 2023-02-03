3b297d8237813887a4db447d7be837aac8a62fb0
gizmore <gizmore@wechall.net>
2023-02-03 06:06:57 +0100

    '7.0.1-r1728 preprocessing production code'

--- GDT_Category.php
+++ GDT_Category.php
@@ -1,40 +1,47 @@
 <?php
 namespace GDO\Category;
+
 use GDO\Core\GDT_Template;
 use GDO\Core\GDT_ObjectSelect;
 
 /**
  * A selection for a Category object.
+ *
  * @author gizmore
- * @see Category
+ * @version 7.0.2
+ * @see GDO_Category
  */
 final class GDT_Category extends GDT_ObjectSelect
 {
-	public function defaultLabel() : self { return $this->label('category'); }
-	
+
+	public function defaultLabel(): self
+	{
+		return $this->label('category');
+	}
+
 	protected function __construct()
 	{
+		parent::__construct();
 		$this->table(GDO_Category::table());
 		$this->icon('folder');
-		$this->emptyLabel = 'sel_no_category';
+		$this->emptyLabel('sel_no_category');
 	}
-	
-	/**
-	 * @return GDO_Category
-	 */
-	public function getCategory()
+
+	public function getCategory(): GDO_Category
 	{
 		return $this->getValue();
 	}
-	
-	public function withCompletion() : self
+
+	public function withCompletion(): self
 	{
-	 	return $this->completionHref(href('Category', 'Completion'));
+		return $this->completionHref(href('Category', 'Completion'));
 	}
-	
-	public function renderHTML() : string
+
+	public function renderHTML(): string
 	{
-		return GDT_Template::php('Category', 'category_html.php', ['field'=>$this]);
+		return GDT_Template::php('Category', 'category_html.php', [
+			'field' => $this
+		]);
 	}
-	
+
 }
